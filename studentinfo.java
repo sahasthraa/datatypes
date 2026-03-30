1cdd73067cfe2cbbbd52b4513ac98240656990a8
sahasthraa <sahasthrathummala@gmail.com>
2026-03-30 05:36:11 -0700

    first commit

---
+++ studentinfo.java
@@ -0,0 +1,18 @@
+public class studentinfo {
+public static void main(String[] args) {
+String name="sahasthra";
+int age=19;
+double percentage=77.7;
+boolean ishostler=false;
+char section='B';
+long phnno=7601070923L;
+int backlogs=0;
+System.out.println("name :"+name);
+System.out.println("age :"+age);
+System.out.println("percentage :"+percentage +"%");
+System.out.println("ishostler:"+ishostler);
+System.out.println("section :"+section);
+System.out.println("phnno :"+phnno);
+System.out.println("backlogs :"+backlogs);
+}
+}
